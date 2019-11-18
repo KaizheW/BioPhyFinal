da97030edd09f8c329e0b36c8da69c9ef7e12dd4
Kaizhe <wkz9528@gmail.com>
2019-11-17 22:25:34 -0500

    update

--- Matlab/mc.m
+++ Matlab/mc.m
@@ -4,13 +4,16 @@
 
 %% ===== Initialization ============================
 % General parameter
-global N L eps;
-N = 20; % Number of Particles
+global N L eps psize KS;
+N = 30; % Number of Particles
 L = 10; % Size of the Domain
+psize = 0.5; % Particle size, assume the same
 eps = 1; % coefficient in Lennard-Jones potential
+KS = 100; % Spring strength
 beta = 1000; % 1/kT
 step_length = 0.5; % MC step moving scale
-nsteps = 1000000;
+step_maxnp = 3; % number of particles that moves each step
+nsteps = 10000000;
 
 % Generate randomly distributed particles
 % D_range = [1, 1]; % Diameter of each particle, range
@@ -19,30 +22,34 @@ nsteps = 1000000;
 
 % Generate a chain
 LB = randi([0,1],N,1); % put a label (A, B, etc.) to each particle.
-D_range = [0.5, 0.5];
-D = rand(N,1).*(D_range(2)-D_range(1)) + D_range(1);
-K = 1*ones(N-1,1); % Spring constant between neighboring particles.
-% X = gausschain(D);
-X = straightchain(D);
+% D_range = [0.5, 0.5];
+% D = rand(N,1).*(D_range(2)-D_range(1)) + D_range(1);
+D = psize * ones(N,1);
+K = KS * ones(N-1,1); % Spring constant between neighboring particles.
+X = psize * self_avoiding_walk; % self-avoiding random walk chain.
+% X = straightchain(D);
 % plot(X(:,1), X(:,2))
 % axis([0 L 0 L]);
 figure
 plot(X(:,1), X(:,2),'-o');
-axis([0 L 0 L]);
+axis([-L/2 L/2 -L/2 L/2]);
 
 % MC initialize
 energy = zeros(nsteps+1,1);
 energy(1) = potential(X,D,K);
-figure
+% figure
 %% MC Loop
 for k = 1:nsteps
-    if mod(k,1000)==0
+    if mod(k,10000)==0
         disp(k);
     end
     u_init = energy(k);
-    dX = step_length.*(2.*rand(1,2)-[1 1]);
-    ind = randi(N);
+    
+    step_np = randi(step_maxnp);
+    dX = step_length.*(2.*rand(step_np,2)-repmat([1 1], step_np, 1));
+    ind = randperm(N,step_np);
     X(ind, :) = X(ind, :) + dX;
+    
     u_final = potential(X,D,K);
     du = u_final - u_init;
     if exp(-beta*du) > rand
@@ -54,15 +61,16 @@ for k = 1:nsteps
     end
     
 %     Plot
-    if mod(k,1000)==0
+    if mod(k,10000)==0
         plot(X(:,1), X(:,2),'-o');
-        axis([0 L 0 L]);
+        axis([-L/2 L/2 -L/2 L/2]);
         drawnow;
     end
     
 end
 figure
 plot(energy)
+
 %% Plot the hist of Force distribution
 % nbins = 1000; % hist plot number of bins
 % fig = figure('Position', [0 0 500 500]);
---
+++ Matlab/self_avoiding_walk.m
@@ -0,0 +1,48 @@
+function trajectory = self_avoiding_walk
+
+% step_num = 20;
+% walk_num = 10;
+global N
+step_num = N-1;
+walk_num = N;
+
+for walk = 1 : walk_num
+
+    x = 0;   
+    y = 0;
+
+    trajectory = zeros ( step_num + 1, 2 );  % the origin.
+    trajectory(1,1:2) = [ x, y ];
+
+    step_length = step_num;
+
+    for step = 1 : step_num    
+
+        neighbors = [ x + 1.0, y; ...
+                    x - 1.0, y; ...
+                    x,       y + 1.0; ...
+                    x,       y - 1.0 ];
+
+        destination = setdiff ( neighbors, trajectory, 'rows' );
+
+        [ nrows, ~ ] = size ( destination );
+
+        if ( nrows == 0 )
+            step_length = step - 1;
+            break;
+        end
+
+        k = ceil ( nrows * rand ( ) );
+        x = destination(k,1);
+        y = destination(k,2);
+
+        trajectory(step+1,1:2) = [ x, y ];
+
+    end
+
+    if ( step_length == step_num )
+        break;
+    end
+
+end
+% plot (trajectory(:,1), trajectory(:,2), 'r-', 'LineWidth', 2 );
